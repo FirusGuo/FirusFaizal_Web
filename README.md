515f4c8d0f03a598d166556ee448bf6161ccef51
firusguo <firusguo@gmail.com>
2017-09-04 18:15:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FirusFaizal_Web
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	//New httprouter
+	router := httprouter.New()
+
+	//Serve Assets Folders
+	router.ServeFiles("/css/*filepath", http.Dir("css"))
+	router.ServeFiles("/js/*filepath", http.Dir("js"))
+	router.ServeFiles("/images/*filepath", http.Dir("images"))
+	router.ServeFiles("/sitemap/*filepath", http.Dir("sitemap"))
+
+	//Routes
+	router.GET("/", Index)
+	router.GET("/robots.txt", robots)
+
+	//Local Server
+	fmt.Println("Listening on Local Server localhost:3000...")
+	panic(http.ListenAndServe(":3000", router))
+
+	//Remote Server
+	// fmt.Println("Listening on Remote Server...")
+	// err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
+	// if err != nil {
+	// 	panic(err)
+	// }
+}
+
+//Index     Handler: Index, Route: /
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		fmt.Println("Error:405:Method is Not GET Method, Handler: Index, Route: / ")
+		return
+	}
+	http.ServeFile(w, r, "index.html")
+}
+
+//robots    Handler: robots, Route: /robots.txt
+func robots(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		fmt.Println("Error:405:Method is Not GET Method, Handler: robots, Route: /robots.txt ")
+		return
+	}
+	http.ServeFile(w, r, "robots.txt")
+}
